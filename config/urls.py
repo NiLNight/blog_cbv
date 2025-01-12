ac0e4479d441b21c9ea386292e403b72015fbb4d
NiLNight <bkmz-2020@inbox.ru>
2025-01-12 19:03:30 +0300

    добавление rss ленты

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
+from apps.blog.feeds import LatestPostsFeed
 
 from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('feeds/latest/', LatestPostsFeed(), name='latest_post_feed'),
     path('', include('apps.blog.urls')),
     path('', include('apps.accounts.urls')),
     path('ckeditor5/', include('django_ckeditor_5.urls'))
